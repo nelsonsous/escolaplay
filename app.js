242fa41be872d40f0cdcd6b5e1693d152a823c7c
Nelson Dias <nelsonsous@gmail.com>
2026-05-14 22:06:19 +0100

    v206: Continuar fica no fundo (Duolingo-style)
    Trocada a ordem no DOM: body -> feedback -> action-bar. Feedback empilha acima do Continuar, ambos colados ao fundo como Duolingo.

--- app.js
+++ app.js
@@ -336,7 +336,7 @@ const YEAR_EXTRA_FILES = {
 };
 
 const _yearExtrasLoaded = {};
-const APP_VERSION = 'v205';
+const APP_VERSION = 'v206';
 // NOTA: a partir da v148, todos os ficheiros _extra*.js são carregados
 // SÍNCRONAMENTE via <script> no index.html. Eliminada a função
 // _loadExtraScript e toda a categoria de bugs "tópicos com 0 exs"
